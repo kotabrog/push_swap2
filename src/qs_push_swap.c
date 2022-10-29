f0f50075936b92dddd298aa2e0cc8f5b225300fe
kotabrog <suzuki.kota0331@gmail.com>
2022-10-29 17:51:30 +0900

    add rotated to rotate_reverse_b_and_push_a

--- src/qs_push_swap.c
+++ src/qs_push_swap.c
@@ -2,25 +2,37 @@
 
 static int split_push_a(t_two_stack *target, int num, int *next_value);
 
-static int rotate_reverse_b_and_push_a(t_two_stack *target, int num, int *next_value)
+static int rotate_reverse_b_and_push_a(t_two_stack *target, int num,
+                                       int *next_value, int rotated)
 {
     int flag;
     int i;
 
     flag = 0;
+    while (!flag && rotated > 0 && *(target->list2->v) == *next_value)
+    {
+        flag = operate_and_add_command(target, COMMAND_PA);
+        flag = flag || operate_and_add_command(target, COMMAND_RA);
+        *next_value += + 1;
+        rotated -= 1;
+    }
     i = num;
     while (!flag && i-- > 0)
     {
         flag = operate_and_add_command(target, COMMAND_RRB);
-        while (!flag && num - i > 0 && *(target->list2->v) == *next_value)
+        while (!flag && num + rotated - i > 0 && *(target->list2->v) == *next_value)
         {
             flag = operate_and_add_command(target, COMMAND_PA);
             flag = flag || operate_and_add_command(target, COMMAND_RA);
             *next_value += + 1;
-            num -= 1;
+            if (num - i > 0)
+                num -= 1;
+            else
+                rotated -= 1;
         }
     }
-    flag = split_push_a(target, num, next_value);
+    if (num + rotated > 0)
+        flag = split_push_a(target, num + rotated, next_value);
     return (flag);
 }
 
@@ -64,7 +76,7 @@ static int split_push_b(t_two_stack *target, int num, int *next_value)
     if (count[0] > 0)
         flag = split_push_a(target, count[0], next_value);
     if (!flag && count[1] > 0)
-        flag = rotate_reverse_b_and_push_a(target, count[1], next_value);
+        flag = rotate_reverse_b_and_push_a(target, count[1], next_value, 0);
     return (flag);
 }
 
@@ -98,6 +110,8 @@ static int split_push_a(t_two_stack *target, int num, int *next_value)
             flag = operate_and_add_command(target, COMMAND_PA);
             count[0] += 1;
         }
+        else if (!is_any_less_value_list(target->list2, num + 1, split_point))
+            break;
         else
         {
             if (len_list(target->list2) >= 2)
@@ -105,10 +119,11 @@ static int split_push_a(t_two_stack *target, int num, int *next_value)
             count[1] += 1;
         }
     }
+    num += 1;
     if (count[0] > 0)
         flag = split_push_b(target, count[0], next_value);
-    if (!flag && count[1] > 0)
-        flag = rotate_reverse_b_and_push_a(target, count[1], next_value);
+    if (!flag && count[1] + num > 0)
+        flag = rotate_reverse_b_and_push_a(target, count[1], next_value, num);
     return (flag);
 }
 
